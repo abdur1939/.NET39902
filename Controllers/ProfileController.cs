7c232a4a10daa5c707794c71e7b23ac34a93f97c
abdur@_1939 <abdur.info24@gmail.com>
2022-05-31 10:14:38 +0600

    Task1_my_CV

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Task1_my_CV.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult myCV()
+        {
+            return View();
+        }
+        public ActionResult Edu()
+        {
+            return View();
+        }
+        public ActionResult Skill()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Task1_my_CV.MvcApplication" Language="C#" %>
